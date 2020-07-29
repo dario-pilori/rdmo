71bc4277239f14f5c8c98e66bd226e831cb11d73
Jochen Klar <mail@jochenklar.de>
2020-07-29 12:52:15 +0200

    Add management django app and refactor import

--- rdmo/core/imports.py
+++ rdmo/core/imports.py
@@ -2,55 +2,10 @@ import logging
 import time
 from random import randint
 
-import defusedxml.ElementTree as ET
+from django.core.exceptions import ValidationError
+from rdmo.core.utils import get_languages
 
-log = logging.getLogger(__name__)
-
-
-# def make_bool(instring):
-#     r = None
-#     s = instring
-#     try:
-#         s = s.decode('utf-8')
-#     except AttributeError:
-#         pass
-#     truelist = ['True', 'true']
-#     falselist = ['False', 'false']
-#     if s in truelist:
-#         r = True
-#     elif s in falselist:
-#         r = False
-#     return r
-
-
-# def get_value_from_treenode(xml_node, element, what_to_get=None):
-#     r = ''
-#     try:
-#         if what_to_get == 'attrib':
-#             r = xml_node.find(element).attrib
-#         elif what_to_get == 'tag':
-#             r = xml_node.find(element).tag
-#         else:
-#             r = xml_node.find(element).text
-#     except Exception as e:
-#         log.debug('Unable to extract "' + element + '" from "' + str(xml_node) + '". ' + str(e))
-#         pass
-#     else:
-#         if r is None:
-#             r = ''
-#         try:
-#             r = r.encode('utf-8', 'ignore')
-#         except Exception as e:
-#             log.debug('Unable to decode string to utf-8: ' + str(e))
-#             pass
-#     return r
-
-
-def generate_tempfile_name():
-    t = int(round(time.time() * 1000))
-    r = randint(10000, 99999)
-    fn = '/tmp/upload_' + str(t) + '_' + str(r) + '.xml'
-    return fn
+logger = logging.getLogger(__name__)
 
 
 def handle_uploaded_file(filedata):
@@ -61,22 +16,94 @@ def handle_uploaded_file(filedata):
     return tempfilename
 
 
-def open_uploaded_file(tempfilename):
-    tempfilename = generate_tempfile_name()
-    return open(tempfilename)
+def generate_tempfile_name():
+    t = int(round(time.time() * 1000))
+    r = randint(10000, 99999)
+    fn = '/tmp/upload_' + str(t) + '_' + str(r) + '.xml'
+    return fn
 
 
-# TODO should be moved to xml.py
-def read_xml_file(filename):
-    tree = None
+def get_instance(element, model):
     try:
-        tree = ET.parse(filename).getroot()
-    except Exception as e:
-        log.error('Xml parsing error: ' + str(e))
-    return tree
+        return model.objects.get(uri=element.get('uri'))
+    except model.DoesNotExist:
+        return model()
 
 
-def set_lang_field(instance, field_name, element, lang_code, lang_field):
-    field = element.get('%s_%s' % (field_name, lang_code))
-    if field:
-        setattr(instance, '%s_%s' % (field_name, lang_field), field)
+def set_common_fields(instance, element):
+    instance.uri_prefix = element.get('uri_prefix') or ''
+    instance.key = element.get('key') or ''
+    instance.comment = element.get('comment') or ''
+
+
+def set_temporary_fields(instance, element):
+    instance.object_name = instance._meta.object_name
+    instance.uri = element.get('uri')
+    instance.warnings = []
+    instance.errors = []
+
+
+def set_lang_field(instance, field_name, element):
+    for lang_code, lang_string, lang_field in get_languages():
+        field = element.get('%s_%s' % (field_name, lang_code))
+        if field:
+            setattr(instance, '%s_%s' % (field_name, lang_field), field)
+
+
+def set_foreign_field(instance, field_name, element, foreign_model):
+    foreign_uri = element.get(field_name)
+    if foreign_uri:
+        try:
+            foreign_field = foreign_model.objects.get(uri=foreign_uri)
+            setattr(instance, field_name, foreign_field)
+        except foreign_model.DoesNotExist:
+            if foreign_model == instance._meta.model:
+                # don't warn if this is a "recursive" foreign key
+                pass
+            else:
+                logger.info('{field_name} {foreign_uri} for {instance_model} {instance_uri} does not exist.'.format(
+                    field_name=field_name,
+                    foreign_uri=foreign_uri,
+                    instance_model=instance._meta.object_name,
+                    instance_uri=instance.uri
+                ))
+                instance.warnings.append('{field_name} {foreign_uri} does not exist.'.format(
+                    field_name=field_name,
+                    foreign_uri=foreign_uri
+                ))
+
+
+def get_m2m_instances(instance, field_name, element, foreign_model):
+    foreign_instances = []
+    for foreign_uri in element.get('field_name', []):
+        try:
+            foreign_instance = foreign_model.objects.get(uri=foreign_uri)
+            foreign_instances.append(foreign_instance)
+        except foreign_model.DoesNotExist:
+            logger.info('{foreign_model} {foreign_uri} for imported {instance_model} {instance_uri} does not exist.'.format(
+                foreign_model=foreign_model._meta.object_name,
+                foreign_uri=foreign_uri,
+                instance_model=instance._meta.object_name,
+                instance_uri=instance.uri
+            ))
+            instance.warnings.append('{foreign_model} {foreign_uri} does not exist.'.format(
+                foreign_model=foreign_model._meta.object_name,
+                foreign_uri=foreign_uri,
+                instance_model=instance._meta.object_name,
+                instance_uri=instance.uri
+            ))
+
+    return foreign_instances
+
+
+def validate_instance(instance, validator_model):
+    try:
+        validator_model(instance).validate()
+    except ValidationError as e:
+        message = '{instance_model} {instance_uri} cannot be imported ({exception}).'.format(
+            instance_model=instance._meta.object_name,
+            instance_uri=instance.uri,
+            exception=e
+        )
+        logger.info(message)
+        instance.errors.append(message)
